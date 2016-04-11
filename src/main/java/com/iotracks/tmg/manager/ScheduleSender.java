80c7d6ad2da3faabf3bea5e7937b010e5b8b55e3
Pavel K <pkazlou@4tegroup.by>
2016-04-11 14:27:21 +0300

    Schedule Sender commit

--- src/main/java/com/iotracks/tmg/manager/ScheduleSender.java
+++ src/main/java/com/iotracks/tmg/manager/ScheduleSender.java
@@ -1,7 +1,54 @@
 package com.iotracks.tmg.manager;
 
-/**
- * Created by pavel on 11.4.16.
- */
+
+import com.iotracks.ws.manager.WebSocketManager;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class ScheduleSender {
+
+    private ScheduledExecutorService mScheduler;
+
+    private int mMessInt;
+    private int mConfInt;
+    private WebSocketManager mWsManager;
+    private String mContId;
+
+
+    public ScheduleSender(int pMessInt, int pConfInt, String pContId, WebSocketManager pWsManager){
+        mMessInt = pMessInt;
+        mConfInt = pConfInt;
+        mWsManager = pWsManager;
+        mContId = pContId;
+        mScheduler = Executors.newScheduledThreadPool(2);
+    }
+
+    public void start(){
+        mScheduler.scheduleWithFixedDelay(new MessageSender(), 0, mMessInt, TimeUnit.SECONDS);
+        mScheduler.scheduleWithFixedDelay(new ConfigSender(), 0, mConfInt, TimeUnit.SECONDS);
+    }
+
+    public void stop(){
+        mScheduler.shutdown();
+    }
+
+    private class MessageSender implements Runnable{
+
+        @Override
+        public void run() {
+            mWsManager.sendMessage(mContId, TMGMessageManager.getRandomMessage().getBytes());
+        }
+    }
+
+    private class ConfigSender implements Runnable{
+
+        @Override
+        public void run() {
+            mWsManager.sendControl(mContId);
+        }
+    }
+
+
 }
